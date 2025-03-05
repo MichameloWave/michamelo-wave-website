06893b02afab3d9bbc7d773dbcc6b5996f9d6a98
MichameloWave <tumeloramphago862@gmail.com>
2025-03-05 05:02:24 -0800

    Update sitemap with new pages

--- app.py
+++ app.py
@@ -5,12 +5,20 @@ app = Flask(__name__)
 
 @app.route('/sitemap.xml')
 def sitemap():
+    # List of pages you want to include in your sitemap
     pages = [
         {'loc': 'https://www.michamelowave.com/', 'lastmod': datetime.date(2025, 3, 5)},
         # Add other pages here
+        {'loc': 'https://www.michamelowave.com/about', 'lastmod': datetime.date(2025, 3, 5)},
+        {'loc': 'https://www.michamelowave.com/music', 'lastmod': datetime.date(2025, 3, 5)},
+        {'loc': 'https://www.michamelowave.com/contact', 'lastmod': datetime.date(2025, 3, 5)},
     ]
+    
+    # Start the XML document
     sitemap_xml = '<?xml version="1.0" encoding="UTF-8"?>'
     sitemap_xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+    
+    # Add each page entry to the sitemap
     for page in pages:
         sitemap_xml += f'''
         <url>
@@ -20,8 +28,13 @@ def sitemap():
             <priority>1.0</priority>
         </url>
         '''
+    
+    # Close the XML document
     sitemap_xml += '</urlset>'
+    
+    # Return the sitemap XML as a response
     return Response(sitemap_xml, content_type='application/xml')
 
+# Ensure the app runs only when executed directly, not on imports
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)  # For production deployment, use appropriate host/port
